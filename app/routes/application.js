ff7e00ba5d28a40d75b1dfee63ed2492381526c9
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-09-01 13:34:52 +0200

    Remove ESA methods from the application route
    These methods are a remnant from the previous setup and are no longer needed.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,3 @@
-import { getOwner } from '@ember/application';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import ENV from 'frontend-public-decisions/config/environment';
@@ -12,35 +11,6 @@ export default class ApplicationRoute extends Route {
   async beforeModel() {
     await this.session.setup();
     return Promise.all([this.startAnalytics(), this.loadCurrentSession()]);
-  }
-
-  async sessionAuthenticated() {
-    await this.loadCurrentSession();
-
-    // The current session needs to be loaded before we start transitioning
-    // because the transitioning depends on the user roles loaded in the current session
-
-    // Since not calling this._super(...arguments) on the first line doesn't work
-    // we copy the implementation ApplicationRouteMixin.sessionAuthenticated here
-    const attemptedTransition = this.session.attemptedTransition;
-    const cookies = getOwner(this).lookup('service:cookies');
-    const redirectTarget = cookies.read('ember_simple_auth-redirectTarget');
-
-    if (attemptedTransition) {
-      attemptedTransition.retry();
-      this.session.attemptedTransition = null;
-    } else if (redirectTarget) {
-      this.router.transitionTo(redirectTarget);
-      cookies.clear('ember_simple_auth-redirectTarget');
-    } else {
-      this.router.transitionTo(this.routeAfterAuthentication);
-    }
-    // End of copy from ApplicationRouteMixin.sessionAuthenticated
-  }
-
-  sessionInvalidated() {
-    const logoutUrl = ENV['torii']['providers']['acmidm-oauth2']['logoutUrl'];
-    window.location.replace(logoutUrl);
   }
 
   loadCurrentSession() {
