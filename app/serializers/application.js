849084b8628b6ae61efb4c76a0365ea2a3d4b9f3
David <david.journo.31@gmail.com>
2022-10-12 16:57:46 +0200

    Adding `DataTableSerializerMixin`

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,1 +1,7 @@
-export { default } from '@ember-data/serializer/json-api';
+/* eslint-disable ember/no-mixins */
+import JSONAPISerializer from '@ember-data/serializer/json-api';
+import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
+
+export default class ApplicationSerializer extends JSONAPISerializer.extend(
+  DataTableSerializerMixin
+) {}
