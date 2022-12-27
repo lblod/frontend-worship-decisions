ab0cd539ba739fcee8db691a1e3c944d56021dbe
felix <ruiz.felixruiz@gmail.com>
2022-12-27 13:09:32 +0100

    Added current-session variant + update session service

--- app/services/session.js
+++ app/services/session.js
@@ -1,7 +1,15 @@
+import { inject as service } from '@ember/service';
 import BaseSessionService from 'ember-simple-auth/services/session';
 import ENV from 'frontend-worship-decisions/config/environment';
 
 export default class SessionService extends BaseSessionService {
+  @service currentSession;
+
+  handleAuthentication(routeAfterAuthentication) {
+    super.handleAuthentication(routeAfterAuthentication);
+    this.currentSession.load();
+  }
+
   handleInvalidation() {
     const logoutUrl = ENV.torii.providers['acmidm-oauth2'].logoutUrl;
     super.handleInvalidation(logoutUrl);
