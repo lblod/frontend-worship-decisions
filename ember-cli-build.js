7e97d082ce563acd67897e2b3d5d55963e61bd76
David Journo <david.journo.31@gmail.com>
2023-05-23 17:51:28 +0200

    Workaround for moment-timezone issue

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const webpack = require('webpack');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
     'ember-simple-auth': {
       useSessionSetupMethod: true,
+    },
+    autoImport: {
+      webpack: {
+        plugins: new webpack.IgnorePlugin({
+          // workaround for https://github.com/embroider-build/ember-auto-import/issues/578
+          resourceRegExp: /moment-timezone/,
+        }),
+      },
     },
   });
   // Use `app.import` to add additional libraries to the generated
