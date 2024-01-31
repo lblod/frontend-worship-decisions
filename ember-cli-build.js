be52a5c126bcf8e6335defa6b1bf78091ee3a1cc
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-01-31 17:04:25 +0100

    Cleanup the ember-cli-build.js file
    These configs are no longer needed on the versions of the packages we use at the moment.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,27 +1,9 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const webpack = require('webpack');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
-    // Add options here
-    'ember-simple-auth': {
-      useSessionSetupMethod: true,
-    },
-    '@appuniversum/ember-appuniversum': {
-      disableWormholeElement: true,
-      dutchDatePickerLocalization: true,
-    },
-    autoImport: {
-      webpack: {
-        plugins: new webpack.IgnorePlugin({
-          // workaround for https://github.com/embroider-build/ember-auto-import/issues/578
-          resourceRegExp: /moment-timezone/,
-        }),
-      },
-    },
-  });
+  const app = new EmberApp(defaults, {});
 
   return app.toTree();
 };
