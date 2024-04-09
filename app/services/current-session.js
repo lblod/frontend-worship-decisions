fda6e08f1872b097560c9f3d816d220e9207e644
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-04-09 16:41:27 +0200

    Allow admin users to impersonate other accounts
    User impersonation allows admin users to see the data of different organisations, which is useful for the controle environment.

--- app/services/current-session.js
+++ app/services/current-session.js
@@ -2,30 +2,44 @@ import Service from '@ember/service';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
+const ADMIN_ROLE = 'LoketLB-admin';
+
 export default class CurrentSessionService extends Service {
+  @service impersonation;
   @service session;
   @service store;
 
   @tracked account;
-  @tracked user;
-  @tracked group;
-  @tracked groupClassification;
+  @tracked _roles = [];
+
+  get user() {
+    return this.account.belongsTo('gebruiker').value();
+  }
+
+  get groupClassification() {
+    return this.group?.belongsTo('classificatie').value();
+  }
 
   async load() {
     if (this.session.isAuthenticated) {
+      await this.impersonation.load();
       let accountId =
         this.session.data.authenticated.relationships.account.data.id;
+
       this.account = await this.store.findRecord('account', accountId, {
-        include: 'gebruiker',
+        include: 'gebruiker.bestuurseenheden.classificatie',
       });
 
-      this.user = await this.account.gebruiker;
-
-      let groupId = this.session.data.authenticated.relationships.group.data.id;
+      // We need to do an extra API call here because ACM/IDM users don't seem to have a "bestuurseenheden" relationship in the DB.
+      // By fetching the record directly we bypass that issue
+      const groupId =
+        this.session.data.authenticated.relationships.group.data.id;
       this.group = await this.store.findRecord('bestuurseenheid', groupId, {
         include: 'classificatie',
+        reload: true,
       });
-      this.groupClassification = await this.group.classificatie;
+
+      this._roles = this.session.data.authenticated.data.attributes.roles;
     }
   }
 
@@ -50,5 +64,13 @@ export default class CurrentSessionService extends Service {
         'http://data.vlaanderen.be/id/concept/BestuurseenheidClassificatieCode/5ab0e9b8a3b2ca7c5e000001',
       ].indexOf(this.groupClassification.uri) > -1
     );
+  }
+
+  get isAdmin() {
+    let roles = this._roles;
+    if (this.impersonation.isImpersonating) {
+      roles = this.impersonation.originalRoles || [];
+    }
+    return roles.includes(ADMIN_ROLE);
   }
 }
