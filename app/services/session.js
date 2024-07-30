78e03bc315d3a2e8bac288d7885a0e779347ea22
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-07-30 11:00:50 +0200

    Retry the transition after login
    When the user tries to access a route when not logged in, the transition will be stored and retried by ESA. However, this only works as long as the user doesn't reload the page. The new ACM/IDM login setup requires a redirect, which causes the transition data to be lost. To work around this we store the url of the route in a cookie, which ESA also checks after login (for FastBoot support). As a result, the transition will now be tried again, even after a page reload.

--- app/services/session.js
+++ app/services/session.js
@@ -4,6 +4,27 @@ import ENV from 'frontend-worship-decisions/config/environment';
 
 export default class SessionService extends BaseSessionService {
   @service currentSession;
+  @service router;
+
+  requireAuthentication(transition, routeOrCallback) {
+    const isAuthenticated = super.requireAuthentication(
+      transition,
+      routeOrCallback
+    );
+
+    // The user isn't authenticated. We store the url in a cookie so we can retry it after logging in.
+    if (!isAuthenticated) {
+      // By default ESA only supports retrying transitions by looking at the `attemptedTransition` property.
+      // However, this property isn't persisted, so after a page reload it is gone, which is an issue for our redirect-based ACM/IDM setup.
+      // ESA also has a cookie-based redirect setup to support FastBoot. With a little bit of code we can hook into this system
+      // so the redirect also survives a page reload.
+      const COOKIE_NAME = 'ember_simple_auth-redirectTarget'; // This is the name that ESA uses: https://github.com/mainmatter/ember-simple-auth/blob/1404c501c2ba9c8c6c071b05f38d42a057522318/packages/ember-simple-auth/src/-internals/routing.js#L37
+      const redirectUrl = routeInfoUrl(transition.to, this.router);
+      document.cookie = `${COOKIE_NAME}=${redirectUrl};path=/;samesite=strict`;
+    }
+
+    return isAuthenticated;
+  }
 
   handleAuthentication(routeAfterAuthentication) {
     super.handleAuthentication(routeAfterAuthentication);
@@ -14,4 +35,29 @@ export default class SessionService extends BaseSessionService {
     const logoutUrl = ENV.torii.providers['acmidm-oauth2'].logoutUrl;
     super.handleInvalidation(logoutUrl);
   }
+}
+
+// This converts a RouteInfo instance to the corresponding url by collecting all
+// the route params and query params and using the router.urlFor method
+// This is a public API reimplementation of the `transition.intent.url` property
+// More info: https://discord.com/channels/480462759797063690/624403666585124895/897832626616926218
+function routeInfoUrl(routeInfo, routerService) {
+  let targetRoute = routeInfo.name;
+  let allRouteParamValues = [];
+  let allRouteQueryParams = {};
+
+  routeInfo.find((routeInfo) => {
+    routeInfo.paramNames.forEach((paramName) => {
+      let paramValue = routeInfo.params[paramName];
+      allRouteParamValues.push(paramValue);
+      allRouteQueryParams = {
+        ...allRouteQueryParams,
+        ...routeInfo.queryParams,
+      };
+    });
+  });
+
+  return routerService.urlFor(targetRoute, ...allRouteParamValues, {
+    queryParams: allRouteQueryParams,
+  });
 }
