fb2fad9d4fd687ca6401903610a7ad75c57e3391
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-11-13 18:02:01 +0100

    Revert the prettier setup for .gjs files
    It causes some issues and we don't really need to edit the .gjs files anyways since we copy them from loket.
    
    Future Ember updates will give us this setup without having to debug the current issues.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,11 @@
 'use strict';
 
 module.exports = {
-  plugins: ['prettier-plugin-ember-template-tag'],
   overrides: [
     {
-      files: '*.{js,ts,gjs,gts}',
+      files: '*.{js,ts}',
       options: {
         singleQuote: true,
-        templateSingleQuote: false,
       },
     },
   ],
