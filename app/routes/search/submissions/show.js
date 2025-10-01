7635c945cbde9eb26c5c71f9cfa5de9fe42f9ca5
Panos Alexopoulos <panos.alexopoulos@triply.cc>
2025-10-01 14:41:26 +0200

    Changing the paths to province information

--- app/routes/search/submissions/show.js
+++ app/routes/search/submissions/show.js
@@ -17,7 +17,7 @@ export default class SearchSubmissionsShowRoute extends Route {
     return this.store.findRecord('submission', params.id, {
       include: [
         'organization.classificatie',
-        'organization.provincie',
+        'organization.primary-site.address.provincie',
         'form-data.types',
         'last-modifier',
       ].join(','),
